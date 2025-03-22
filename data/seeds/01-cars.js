733232ac3c601b39d95d3c0c6e1ae59816bb3e85
Lun Ng <lunngbill@gmail.com>
2025-03-22 16:19:46 -0700

    update1

--- data/seeds/01-cars.js
+++ data/seeds/01-cars.js
@@ -1,1 +1,29 @@
 // STRETCH
+const cars = [
+    {
+        vin: "1111111111111",
+        make: "toyota",
+        model: "prius",
+        mileage: 215000,
+        title: "clean",
+        transmission: "manual",
+    },
+    {
+        vin: "1111111111111",
+        make: "toyota",
+        model: "corolla",
+        mileage: 115000,
+        title: "salvage",
+    },
+    {
+        vin: "1111111111111",
+        make: "ford",
+        model: "focus",
+        mileage: 15000,
+    }
+]
+
+exports.seed = async function(knex) {
+    await knex('cars').truncate()
+    await knex('cars').insert(cars)
+}
